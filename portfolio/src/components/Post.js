63fda69a2bbefe31c231aef4a4532346615fe783
Raveen Panditha <raveenpanditha9@gmail.com>
2022-07-25 13:10:10 +0530

    new comment

--- portfolio/src/components/Post.js
+++ portfolio/src/components/Post.js
@@ -21,7 +21,7 @@ export default function Post() {
         }`).then((data) => setPost(data))
         .catch(console.error)
     }, []);
-
+//sdfasdfs
   return(
     <main className='bg-green-100 min-h-screen p-12'>
       <section className='container mx-auto'>
